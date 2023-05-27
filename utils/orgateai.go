5c123b99094cc1c9851a6a3a3dc1f0460e3cd9d9
sub0ne1 <iam0verread@gmail.com>
2023-05-26 23:59:00 -0400

    API testing: TODO

--- utils/orgateai.go
+++ utils/orgateai.go
@@ -4,10 +4,9 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"net/http"
 )
-
-// postBody, _ := []byte()
 
 const url = ""
 
@@ -23,18 +22,33 @@ func formPostCallBody(query string, apiKey string) postBody {
 	return body
 }
 
-func main(query string, apiKey string) {
+func main(query string, apiKey string) []byte {
 	reqBody := formPostCallBody(query, apiKey)
 	body, err := json.Marshal(reqBody)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
+
 	fmt.Println(body)
-	res, err := http.NewRequest("POST", url, bytes.NewBuffer(body))
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(body))
 	if err != nil {
 		panic(err)
 	}
-	defer res.Body.Close()
-	fmt.Print("Coming here")
+
+	req.Header.Add("Accept", "application/json")
+	req.Header.Add("User-Agent", "")
+	resp, err := http.DefaultClient.Do(req)
+
+	if err != nil {
+		panic(err)
+	}
+
+	respBytes, err := ioutil.ReadAll(resp.Body)
+
+	if err != nil {
+		panic(err)
+	}
+
+	return respBytes
 }
